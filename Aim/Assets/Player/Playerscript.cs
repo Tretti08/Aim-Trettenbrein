5edbb5b6aa3c2a09ab32a258853f7afd9bf90863
Humle7113 <137985601+Humle7113@users.noreply.github.com>
2023-09-20 14:33:47 +0200

    first movement

---
+++ Aim/Assets/Player/Playerscript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float verticalInput = Input.GetAxis("Vertical");
+       float horizontalInput = Input.GetAxis("Horizontal");
+
+       transform.position += transform.forward * verticalInput;
+       transform.position += transform.right * horizontalInput;
+
+    }
+}
